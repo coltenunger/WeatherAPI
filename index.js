9b143b0b23038c984985531bc2beb5d5380e5799
Colten Unger <coltenunger@gmail.com>
2024-01-12 20:37:35 -0600

    Modify changes to style and index.js file and add utils.js file

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import axios from "axios";
 import bodyParser from "body-parser";
-import 'dotenv/config';
+import "dotenv/config";
+// Import functions from utils.js
+import { metersToMiles, getFormattedDate } from "./utils.js";
 
 const app = express();
 const port = 3000;
@@ -18,20 +20,29 @@ app.get("/", (req, res) => {
 app.get('/get-weather', async (req, res) => {
     try {
         const city = req.query.city;
-        // const state = req.query.state;
+        const state = req.query.state;
         const country = req.query.country
-        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&limit=1&units=imperial&appid=${API_KEY}`);
-        const tempRaw = response.data.main.temp;
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city},${state},${country}&limit=1&units=imperial&appid=${API_KEY}`);
+        const data = response.data;
+        const tempRaw = data.main.temp;
         const tempRound = Math.round(tempRaw);
-        const weatherRaw = response.data.weather[0].description;
-        const weatherUpper = weatherRaw.charAt(0).toUpperCase() + weatherRaw.slice(1)
+        const weatherRaw = data.weather[0].description;
+        const weatherUpper = weatherRaw.charAt(0).toUpperCase() + weatherRaw.slice(1);
+        const windSpeedRaw = data.wind.speed;
+        const windSpeedRound = windSpeedRaw.toFixed(1);
+        const formattedDate = getFormattedDate();
+        const visibilityInMiles = metersToMiles(data.visibility);
         res.render("weather.ejs", {
-            data: response.data,
+            data: data,
+            temperature: tempRound,
             weatherDesc: weatherUpper,
-            cityName: response.data.name,
-            // stateName: state,
+            windSpeed: windSpeedRound,
+            humidity: data.main.humidity,
+            visibility: visibilityInMiles,
+            cityName: data.name,
+            stateName: state,
+            currentDate: formattedDate,
             countryName: response.data.sys.country,
-            temperature: tempRound
         });
     } catch (error) {
         res.render("weather.ejs", { error: JSON.stringify(error.response.data) });
